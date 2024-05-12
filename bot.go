51791b73f383fac82fa011a78e95dcfccc1414fe
CΛƬ <98459136+xcatbear@users.noreply.github.com>
2024-05-12 08:16:17 +0200

    Update bot.go

--- bot.go
+++ bot.go
@@ -8,23 +8,18 @@ import (
 	"time"
 )
 
-const (
-	requestsPerSecond = 500
-	url               = "https://camo.githubusercontent.com/ccabda83743448d75055da131db332c2d62620cf73b695bdcb67dd00a7423b43/68747470733a2f2f6170692e76697369746f7262616467652e696f2f6170692f56697369746f724869743f757365723d786361746265617226636f756e74436f6c6f72636f756e74436f6c6f7226636f756e74436f6c6f723d253233303036454646"
-)
-
 var totalSuccessfulRequests int64
 
-func makeRequests() {
+func makeRequests(url string, requestsPerSecond int) {
 	for {
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Printf("Error making request: %v\n", err)
 		} else {
 			atomic.AddInt64(&totalSuccessfulRequests, 1)
-			resp.Body.Close()
+			resp.Body.Close() 
 		}
-		time.Sleep(time.Second / requestsPerSecond)
+		time.Sleep(time.Second / time.Duration(requestsPerSecond))
 	}
 }
 
@@ -37,12 +32,21 @@ func printProgress() {
 }
 
 func main() {
+	var url string
+	var requestsPerSecond int
+
+	fmt.Print("Enter the URL: ")
+	fmt.Scanln(&url)
+
+	fmt.Print("Enter the amount of Threads: ")
+	fmt.Scanln(&requestsPerSecond)
+
 	var wg sync.WaitGroup
 	for i := 0; i < requestsPerSecond; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			makeRequests()
+			makeRequests(url, requestsPerSecond)
 		}()
 	}
 
